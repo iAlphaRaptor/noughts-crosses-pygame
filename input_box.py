632f7af5000e753d054f94e0fdc207987fe5e4fb
iAlphaRaptor <maximwaring@yahoo.co.uk>
2020-06-22 15:19:44 +0100

    Added more colour functionality to input box

--- input_box.py
+++ input_box.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame, random
 pygame.init()
 
 boxFont = pygame.font.Font("Fonts/font.ttf", 40)
@@ -11,6 +11,16 @@ class InputBox(pygame.sprite.Sprite):
         self.colour = colour
         self.boxColour = boxColour
         self.textColour = textColour
+        self.inactiveColour = boxColour
+        temp = []
+        for i in range(3):
+            if self.boxColour[i] < 100:
+                temp.append(self.boxColour[i] + 30)
+            else:
+                temp.append(self.boxColour[i] - random.randint(25, 100))
+        print(temp)
+        self.activeColour = (temp[0], temp[1], temp[2])
+
         self.questionText = text
         self.text = text
         self.inputText = ""
@@ -24,15 +34,24 @@ class InputBox(pygame.sprite.Sprite):
         self.boxText = boxFont.render(self.inputText, True, textColour, None)
 
         pygame.draw.rect(self.image, colour, (0, 0, int(len(text)*20), 120))
-        self.image.blit(self.outputText, (60,10))
+        self.image.blit(self.outputText, (10,10))
         pygame.draw.rect(self.image, boxColour, (10, 60, int((len(text)*20) - 20), 50))
         self.image.blit(self.boxText, (20, 60))
 
         self.active = False
 
     def isClicked(self, mousex, mousey):
-        self.active = True
-        return self.rect.collidepoint(mousex, mousey)
+        if self.rect.collidepoint(mousex, mousey):
+            self.active = True
+            self.boxColour = self.activeColour
+        else:
+            self.active = False
+            self.boxColour = self.inactiveColour
+
+        pygame.draw.rect(self.image, self.colour, (0, 0, int(len(self.text)*20), 120))
+        self.image.blit(self.outputText, (10,10))
+        pygame.draw.rect(self.image, self.boxColour, (10, 60, int((len(self.text)*20) - 20), 50))
+        self.image.blit(self.boxText, (20, 60))
 
     def enterText(self,char):
         if char == pygame.K_BACKSPACE:
@@ -43,6 +62,6 @@ class InputBox(pygame.sprite.Sprite):
 
         self.image.fill(WHITE)
         pygame.draw.rect(self.image, self.colour, (0, 0, int(len(self.text)*20), 120))
-        self.image.blit(self.outputText, (60,10))
+        self.image.blit(self.outputText, (10,10))
         pygame.draw.rect(self.image, self.boxColour, (10, 60, int((len(self.text)*20) - 20), 50))
         self.image.blit(self.boxText, (20, 60))
